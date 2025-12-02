0185fc8d40de534a4c73c944332376d5c2a9fe73
Pascal-Joos <pascal.joos@outlook.de>
2025-12-02 15:38:55 +0000

    fix: 16 - ASSIGN_FIELD_NULLABLE - this.eurekaHttpClient = newEurekaHttpClient; - assigning @Nullable expression to @NonNull field

--- eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
@@ -95,7 +95,7 @@ public class RemoteRegionRegistry implements LookupService<String> {
       new AtomicReference<>(new Applications());
   private final EurekaServerConfig serverConfig;
   private volatile boolean readyForServingData;
-  private final EurekaHttpClient eurekaHttpClient;
+  @Nullable private final EurekaHttpClient eurekaHttpClient;
   private long timeOfLastSuccessfulRemoteFetch = System.currentTimeMillis();
   private long deltaSuccesses = 0;
   private long deltaMismatches = 0;
@@ -424,6 +424,9 @@ public class RemoteRegionRegistry implements LookupService<String> {
         delta);
 
     if (shouldUseExperimentalTransport()) {
+      if (eurekaHttpClient == null) {
+        return null;
+      }
       try {
         EurekaHttpResponse<Applications> httpResponse =
             delta ? eurekaHttpClient.getDelta() : eurekaHttpClient.getApplications();
