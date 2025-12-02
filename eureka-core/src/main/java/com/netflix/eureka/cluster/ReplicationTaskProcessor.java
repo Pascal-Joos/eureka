c74d0f27a38ba4d354490f56bdd9502ae87749f2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-02 14:08:23 +0000

    Revert "fix: 7 - DEREFERENCE_NULLABLE - String message = e.getMessage().toLowerCase(); - dereferenced expression e.getMessage() is @Nullable"
    This reverts commit f28b16701b8c54f708938fb4e9b662dd3c90f15a.

--- eureka-core/src/main/java/com/netflix/eureka/cluster/ReplicationTaskProcessor.java
+++ eureka-core/src/main/java/com/netflix/eureka/cluster/ReplicationTaskProcessor.java
@@ -214,7 +214,7 @@ class ReplicationTaskProcessor implements TaskProcessor<ReplicationTask> {
   private static boolean maybeReadTimeOut(Throwable e) {
     do {
       if (IOException.class.isInstance(e)) {
-        String message = (e.getMessage() == null ? "" : e.getMessage().toLowerCase());
+        String message = e.getMessage().toLowerCase();
         Matcher matcher = READ_TIME_OUT_PATTERN.matcher(message);
         if (matcher.find()) {
           return true;
