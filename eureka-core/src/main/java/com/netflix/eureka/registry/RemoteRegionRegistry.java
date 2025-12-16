37cd9af6f005ab32bfe9ca65bf214c9f055ab591
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:05:14 +0000

    Revert "fix: 16 - ASSIGN_FIELD_NULLABLE - this.eurekaHttpClient = newEurekaHttpClient; - assigning @Nullable expression to @NonNull field"
    This reverts commit 3be4816c137cc9deec080dadd7bf469c18ebf661.

--- eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
@@ -156,11 +156,15 @@ public class RemoteRegionRegistry implements LookupService<String> {
     discoveryApacheClient.addFilter(new EurekaIdentityHeaderFilter(identity));
 
     // Configure new transport layer (candidate for injecting in the future)
-    EurekaHttpClient newEurekaHttpClient;
-    ClusterResolver clusterResolver = StaticClusterResolver.fromURL(regionName, remoteRegionURL);
-    newEurekaHttpClient =
-        EurekaServerHttpClients.createRemoteRegionClient(
-            serverConfig, clientConfig.getTransportConfig(), serverCodecs, clusterResolver);
+    EurekaHttpClient newEurekaHttpClient = null;
+    try {
+      ClusterResolver clusterResolver = StaticClusterResolver.fromURL(regionName, remoteRegionURL);
+      newEurekaHttpClient =
+          EurekaServerHttpClients.createRemoteRegionClient(
+              serverConfig, clientConfig.getTransportConfig(), serverCodecs, clusterResolver);
+    } catch (Exception e) {
+      logger.warn("Transport initialization failure", e);
+    }
     this.eurekaHttpClient = newEurekaHttpClient;
 
     try {
