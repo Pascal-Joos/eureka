630d202bb29adbbc00ff25bfb771a8c6cd029e43
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:45:46 +0000

    fix: 5 - RETURN_NULLABLE - return resourceRecordSet; - returning @Nullable expression from method with @NonNull return type

--- eureka-core/src/main/java/com/netflix/eureka/aws/Route53Binder.java
+++ eureka-core/src/main/java/com/netflix/eureka/aws/Route53Binder.java
@@ -351,6 +351,9 @@ public class Route53Binder implements AwsBinder {
     }
 
     public ResourceRecordSet getResourceRecordSet() {
+      if (resourceRecordSet == null) {
+        throw new IllegalStateException("resourceRecordSet is null");
+      }
       return resourceRecordSet;
     }
   }
