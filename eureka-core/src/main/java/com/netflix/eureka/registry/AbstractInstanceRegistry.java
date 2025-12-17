d7154695817f8c3adc3620f8a0efb5c649075a91
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:16:38 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='enhanced-for expression remoteRegions is @Nullable' /home/vscode/nullness-benchmarks/eureka/eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java:812       for (String remoteRegion : remoteRegions) {

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -812,36 +812,39 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
       }
     }
     if (includeRemoteRegion) {
-      for (String remoteRegion : remoteRegions) {
-        RemoteRegionRegistry remoteRegistry = regionNameVSRemoteRegistry.get(remoteRegion);
-        if (null != remoteRegistry) {
-          Applications remoteApps = remoteRegistry.getApplications();
-          for (Application application : remoteApps.getRegisteredApplications()) {
-            if (shouldFetchFromRemoteRegistry(application.getName(), remoteRegion)) {
-              logger.info(
-                  "Application {}  fetched from the remote region {}",
-                  application.getName(),
-                  remoteRegion);
+      String[] nonNullRemoteRegions = remoteRegions;
+      if (nonNullRemoteRegions != null) {
+        for (String remoteRegion : nonNullRemoteRegions) {
+          RemoteRegionRegistry remoteRegistry = regionNameVSRemoteRegistry.get(remoteRegion);
+          if (null != remoteRegistry) {
+            Applications remoteApps = remoteRegistry.getApplications();
+            for (Application application : remoteApps.getRegisteredApplications()) {
+              if (shouldFetchFromRemoteRegistry(application.getName(), remoteRegion)) {
+                logger.info(
+                    "Application {}  fetched from the remote region {}",
+                    application.getName(),
+                    remoteRegion);
 
-              Application appInstanceTillNow =
-                  apps.getRegisteredApplications(application.getName());
-              if (appInstanceTillNow == null) {
-                appInstanceTillNow = new Application(application.getName());
-                apps.addApplication(appInstanceTillNow);
+                Application appInstanceTillNow =
+                    apps.getRegisteredApplications(application.getName());
+                if (appInstanceTillNow == null) {
+                  appInstanceTillNow = new Application(application.getName());
+                  apps.addApplication(appInstanceTillNow);
+                }
+                for (InstanceInfo instanceInfo : application.getInstances()) {
+                  appInstanceTillNow.addInstance(instanceInfo);
+                }
+              } else {
+                logger.debug(
+                    "Application {} not fetched from the remote region {} as there exists a "
+                        + "whitelist and this app is not in the whitelist.",
+                    application.getName(),
+                    remoteRegion);
               }
-              for (InstanceInfo instanceInfo : application.getInstances()) {
-                appInstanceTillNow.addInstance(instanceInfo);
-              }
-            } else {
-              logger.debug(
-                  "Application {} not fetched from the remote region {} as there exists a "
-                      + "whitelist and this app is not in the whitelist.",
-                  application.getName(),
-                  remoteRegion);
             }
+          } else {
+            logger.warn("No remote registry available for the remote region {}", remoteRegion);
           }
-        } else {
-          logger.warn("No remote registry available for the remote region {}", remoteRegion);
         }
       }
     }
