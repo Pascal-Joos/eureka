569d497b47949028196defc63bd529ae583a7d32
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 15:39:46 +0000

    Revert "fix: 24 - DEREFERENCE_NULLABLE - long delay = System.currentTimeMillis() - nextHolder.getSubmitTimestamp(); - dereferenced expression nextHolder is @Nullable"
    This reverts commit 8798e0858f5d6c82ec6083d738699a2dc5c455ea.

--- eureka-core/src/main/java/com/netflix/eureka/util/batcher/AcceptorExecutor.java
+++ eureka-core/src/main/java/com/netflix/eureka/util/batcher/AcceptorExecutor.java
@@ -10,7 +10,6 @@ import com.netflix.servo.monitor.Monitors;
 import com.netflix.servo.monitor.StatsTimer;
 import com.netflix.servo.monitor.Timer;
 import com.netflix.servo.stats.StatsConfig;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
@@ -362,8 +361,7 @@ class AcceptorExecutor<ID, T> {
       }
 
       TaskHolder<ID, T> nextHolder = pendingTasks.get(processingOrder.peek());
-      long delay =
-          System.currentTimeMillis() - Nullability.castToNonnull(nextHolder).getSubmitTimestamp();
+      long delay = System.currentTimeMillis() - nextHolder.getSubmitTimestamp();
       return delay >= maxBatchingDelay;
     }
   }
