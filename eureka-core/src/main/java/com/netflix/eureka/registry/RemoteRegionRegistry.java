c16b90d0f745258d63377485152ef1f797df6a1d
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 15:28:13 +0000

    Revert "fix: 19 - DEREFERENCE_NULLABLE - for (Application app : applications.get().getRegisteredApplications()) { - dereferenced expression applications.get() is @Nullable"
    This reverts commit 9beccb3595ecc57d40ff6f2261a1253ebbe85a54.

--- eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
@@ -42,7 +42,6 @@ import com.netflix.servo.monitor.Stopwatch;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.filter.GZIPContentEncodingFilter;
 import com.sun.jersey.client.apache4.ApacheHttpClient4;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -529,8 +528,7 @@ public class RemoteRegionRegistry implements LookupService<String> {
   public List<InstanceInfo> getInstancesById(String id) {
     List<InstanceInfo> list = new ArrayList<>(1);
 
-    for (Application app :
-        Nullability.castToNonnull(applications.get()).getRegisteredApplications()) {
+    for (Application app : applications.get().getRegisteredApplications()) {
       InstanceInfo info = app.getByInstanceId(id);
       if (info != null) {
         list.add(info);
