60797713272c08685726a09fe4fbd62b1c21dc17
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 15:20:07 +0000

    fix: 17 - DEREFERENCE_NULLABLE - return this.applications.get().getRegisteredApplications(appName); - dereferenced expression this.applications.get() is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
@@ -42,6 +42,7 @@ import com.netflix.servo.monitor.Stopwatch;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.filter.GZIPContentEncodingFilter;
 import com.sun.jersey.client.apache4.ApacheHttpClient4;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -521,7 +522,7 @@ public class RemoteRegionRegistry implements LookupService<String> {
 
   @Override
   public Application getApplication(String appName) {
-    return this.applications.get().getRegisteredApplications(appName);
+    return Nullability.castToNonnull(this.applications.get()).getRegisteredApplications(appName);
   }
 
   @Override
